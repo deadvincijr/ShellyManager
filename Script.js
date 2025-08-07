ab533f3a6d65eaf208dc6b62463f926c60b8fa63
deadvincijr <deankwebb@gmail.com>
2025-08-07 15:45:09 -0600

    Add files via upload
    Adding the website files to the repository

---
+++ Script.js
@@ -0,0 +1,17 @@
+function DownloadBitcoin() {
+    window.open("https://drive.google.com/uc?export=download&id=1PjkVRXVtFhI6zFG13skZcYjXf7nx2mxB", "_blank");
+}
+function DownloadRooms() {
+    window.open("https://drive.google.com/uc?export=download&id=1d9KRdIgFURJ8P_S-xFNBkOg2Osm5IDgB")
+}
+function DownloadParrot() {
+    window.open("https://drive.google.com/uc?export=download&id=1VHEshmjsFp6Log1iTirO6lTnH1ZS4QvU")
+}
+
+function DownloadNootNoot() {
+    window.open("https://drive.google.com/uc?export=download&id=1W7-rlgT0kJEb_Xi_g_NbQcuBsirysYse")
+}
+
+function DownloadA90() {
+    window.open("https://drive.google.com/uc?export=download&id=1KEEqDa43Yu6dZyX0qPd3vawEjzGZSAob")
+}
